a2bb4af151b427a38121b2ac1b0b58cbafd47180
vm mac <vmmac@vms-Mac.local>
2016-12-08 04:03:13 +0800

    initial commit

--- randomapproach/ViewController.swift
+++ randomapproach/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("launch app")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
